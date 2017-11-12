e49f220f1a3dd5fd3e5e9d0fd44f19ebbfbbed1c
nikkiedd <dolot_nicole@yahoo.com>
2017-11-12 14:55:46 +0200

    added predicate

---
+++ insertionSort.pl
@@ -0,0 +1,18 @@
+%Insert an element in the right place in a sorted list
+%insert(L:list,E:elem,F:list)
+%insert(i,i,o)
+
+insert([],E,[E]).
+insert([H|T],E,[H|T1]):-E>H,
+    					insert(T,E,T1).
+insert([H|T],E,[E,H|T]):- E=<H.
+
+%Insertion Sort(with
+%out removing the duplicates)
+%insertionSort(L:list, S:list)
+%insertionSort(i,o)
+
+insertionSort(L,S):-insertionSort(L,[],S).
+insertionSort([],S,S).
+insertionSort([H|T],Acc,S):- insert(Acc,H,Partial),	%Acc is an accumulator(collector variable)- it holds the part of the list which has been sorted so far
+    						insertionSort(T,Partial,S).
