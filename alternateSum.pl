f1fc78597fa67254cd092026dfbe0e9c8960daa9
nikkiedd <dolot_nicole@yahoo.com>
2017-11-12 13:28:13 +0200

    new predicate

--- alternateSum.pl
+++ alternateSum.pl
@@ -4,8 +4,8 @@
 %
 altSum(L,S):-altSum(L,1,S).
 altSum([],_,0).
-altSum([H|T],P,S):- P mod 2 =\= 0,
-    				P1 is P+1,
+altSum([H|T],P,S):- P mod 2 =\= 0,		%P represents the current position in the list
+    				P1 is P+1,	
     				altSum(T,P1,S1),
     				S is S1+H.
 altSum([H|T],P,S):- P mod 2 =:= 0,
