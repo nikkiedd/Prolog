781d37a736efa2e2d2b5cf54e3a5de8639d524cb
nikkiedd <dolot_nicole@yahoo.com>
2017-11-15 19:58:31 +0200

    with/without allowing duplicates

--- merge2Lists.pl
+++ merge2Lists.pl
@@ -7,6 +7,22 @@ merge([],[],[]).
 merge([],L,L).
 merge(L,[],L).
 merge([H1|T1],[H2|T2],[H1|T3]):- H1=<H2,
-    							 merge(T1,[H2|T2],T3).
+    				 merge(T1,[H2|T2],T3).
 merge([H1|T1],[H2|T2],[H2|T3]):- H1>H2,
-    							 merge([H1|T1],T2,T3).
+    				 merge([H1|T1],T2,T3).
+
+%Merge two lists sorted ascendingly(remove the duplicates).
+
+%merge(A:list, B:list, C:list)
+%merge(i,i,o)
+
+merge([],[],[]).
+merge([],L,L).
+merge(L,[],L).
+merge([H1|T1],[H2|T2],[H1|T3]):- H1<H2,
+    				 merge(T1,[H2|T2],T3).
+merge([H1|T1],[H2|T2],[H2|T3]):- H1>H2,
+    				 merge([H1|T1],T2,T3).
+merge([H1|T1],[H2|T2],[H2|T3]):- H1=:=H2,
+				 merge(T1,T2,T3).	% lose H1. We don't need it.
+
