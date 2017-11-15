22d95c76b4bef5e27ee1ba77add28650565366b3
nikkiedd <dolot_nicole@yahoo.com>
2017-11-15 18:08:00 +0200

    added predicate

---
+++ valleyList.pl
@@ -0,0 +1,15 @@
+%Find if a given list is of type 'valley'
+%e.g. [10,8,4,2,-1,7,90] is of type valley; the elems descend and then ascend.
+%No consecutive duplicates. No fluctuations.
+
+%isValley(L:list, reachedBottom:0 or 1). 0 indicates descent, 1 ascent
+
+isValley(L):-isValley(L,0).	% wrapper function
+isValley([H1,H2|T], 0):- H1>H2,	% we've reached the bottom of the valley(H2)
+    					isValley([H2|T],1).
+isValley([H1,H2|T], 0) :- H1>H2,
+    					  isValley([H2|T],0).
+isValley([H1,H2|T],1) :- H1<H2,
+    					isValley([H2|T],1).
+isValley([_],1).
+isValley([H1,H2|_],_):- H1=:=H2, false.	% a valley can't have consecutive duplicates
