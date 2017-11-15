54bf4c0a60be7297f7ab5b85530cd584a73b39fe
nikkiedd <dolot_nicole@yahoo.com>
2017-11-15 18:12:40 +0200

    added predicate

--- valleyList.pl
+++ valleyList.pl
@@ -5,11 +5,12 @@
 %isValley(L:list, reachedBottom:0 or 1). 0 indicates descent, 1 ascent
 
 isValley(L):-isValley(L,0).	% wrapper function
-isValley([H1,H2|T], 0):- H1>H2,	% we've reached the bottom of the valley(H2)
-    					isValley([H2|T],1).
+isValley([H1,H2,H3|T], 0):- H1>H2,	% we've reached the bottom of the valley(H2)
+    			    H2<H3,
+    			    isValley([H2,H3|T],1).
 isValley([H1,H2|T], 0) :- H1>H2,
-    					  isValley([H2|T],0).
+    			  isValley([H2|T],0).
 isValley([H1,H2|T],1) :- H1<H2,
-    					isValley([H2|T],1).
+    		         isValley([H2|T],1).
 isValley([_],1).
 isValley([H1,H2|_],_):- H1=:=H2, false.	% a valley can't have consecutive duplicates
