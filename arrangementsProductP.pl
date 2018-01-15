2f48e0769707e737eead772454e16272c8afd1a0
nikkiedd <dolot_nicole@yahoo.com>
2018-01-15 20:03:19 +0200

    made it work

--- arrangementsProductP.pl
+++ arrangementsProductP.pl
@@ -1,12 +1,14 @@
 %Generate all arrangements of K elements from a list L having the product P.
 %arr(L:list, K:natural number, P:integer, R:list)
 %arr(i,i,i,o)
-arr([E|_],1,E,[E]).
-arr([H|T],K,P,[H|R]):- K1 is K-1,
-    					P1 is P/H,
-    					arr(T,K1,P1,R).
-arr([_|T],K,P,R):-arr(T,K,P,R).
+arr(L,1,E,[E]):-rem(L,E,_).
+arr(L,K,P,[E|R]):- K1 is K-1,
+    			   rem(L,E,Rest),
+    				P1 is P/E,
+    				arr(Rest,K1,P1,R).
 
+rem([H|T],H,T).
+rem([H|T],E,[H|R]):-rem(T,E,R).
 
 
 allArr(L,K,P,Res):-findall(R,arr(L,K,P,R),Res).
